9c2f8003d774db3325b6b82cdc34d5dd3c88581e
SungChul23 <kimsam0923@naver.com>
2026-07-28 14:28:22 +0900

    Feat: 퍼블릭 서브넷 구성

--- tf-step3/vpc.tf
+++ tf-step3/vpc.tf
@@ -9,3 +9,21 @@ resource "aws_vpc" "de-ai-22-company" {
     Name = "de-ai-22-company"
   }
 }
+
+# 서브넷
+resource "aws_subnet" "public" {
+  # 암묵적 의존성 -> 서브넷 구성을 위해서는 반드시 VPC가 있어야함 , 즉 create First
+  vpc_id = aws_vpc.de-ai-22-company.id
+
+  # 앞 3자리 고정 -> 256개 가용 가능
+  cidr_block = "10.0.1.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  map_public_ip_on_launch = true
+
+  # 태그 지정
+  tags = {
+    Name = "de-ai-22-public-subnet"
+  }
+}
