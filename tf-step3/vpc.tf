96e9cfa8826b6390f41094065304e47086c5fb68
SungChul23 <kimsam0923@naver.com>
2026-07-28 14:45:31 +0900

    Feat: 라우트 테이블 구성 및 서브넷 바인딩

--- tf-step3/vpc.tf
+++ tf-step3/vpc.tf
@@ -1,5 +1,6 @@
 # 특정 기업/개인/단체등 전용 VPC 생성 선언
 
+# VPC 생성
 resource "aws_vpc" "de-ai-22-company" {
   # CIDR 규칙 지정 65536개 IP를 구성할수 있다. 10.0.0.0/16
   cidr_block = "10.0.0.0/16"
@@ -10,7 +11,7 @@ resource "aws_vpc" "de-ai-22-company" {
   }
 }
 
-# 서브넷
+# 서브넷 생성
 resource "aws_subnet" "public" {
   # 암묵적 의존성 -> 서브넷 구성을 위해서는 반드시 VPC가 있어야함 , 즉 create First
   vpc_id = aws_vpc.de-ai-22-company.id
@@ -27,3 +28,36 @@ resource "aws_subnet" "public" {
     Name = "de-ai-22-public-subnet"
   }
 }
+
+# 인터넷 GW 생성
+resource "aws_internet_gateway" "company" {
+
+  vpc_id = aws_vpc.de-ai-22-company.id
+
+  tags = {
+    Name = "de-ai-22-company-igw"
+  }
+}
+
+# 라우팅 테이블 생성
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.de-ai-22-company.id
+  route {
+
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.company.id
+  }
+
+  tags = {
+    Name = "de-ai-22-company-public-rt"
+  }
+
+}
+
+# 가장 중요 : 라우팅 테이블을 서브넷에 바인딩
+resource "aws_route_table_association" "public" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
+}
+
+
