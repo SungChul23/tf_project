834c75b43bb403ebf537e2133cc4eaad0951c5ef
SungChul23 <kimsam0923@naver.com>
2026-08-03 15:18:51 +0900

    feat: AWS CloudWatch Logs 설정 추가 및 README 구조 설계 업데이트

---
+++ tf-step6/infra/07_logging.tf
@@ -0,0 +1,12 @@
+# AWS CloudWatch Logs 관련 설정
+
+# k8s 컨트롤 플랜의 로그를 CloudWatch Logs로 보내기 위해 Log Group 생성
+resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
+  name              = "/aws/eks/${local.cluster_name}/cluster"
+  retention_in_days = 7
+
+  tags ={
+    Name = "${local.cluster_name}-cluster-logs"
+  }
+  
+}
